5f207d30f77d651218ec5f922644f53e39210e50
Harish0402 <ja5858786@gmail.com>
2025-01-19 08:24:42 +0530

    ready for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
-import { HashRouter } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // Correct import for React 18
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
     <App />
-  </HashRouter>
+  </React.StrictMode>
 );
