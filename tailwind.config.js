7edd5e4ef9c3d57fa512d2b711a7dbfe11dd8d34
Harish0402 <ja5858786@gmail.com>
2025-01-19 08:03:58 +0530

    ready for deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      appearance: ['responsive', 'hover', 'focus'],
+    },
     fontFamily:{
       'display':'sriracha'
     },
